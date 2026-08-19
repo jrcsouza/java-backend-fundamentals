1cc21e6b6306ddb14eb94a694bcfa3dabf70e2e2
Junio das Chagas de Souza <juniochagas@gmail.com>
2026-08-19 13:55:04 -0300

    feat: resolve Beecrowd 1004 - Produto Simples

---
+++ src/main/java/com/github/jrcsouza/fundamentals/m01_fundamentals/ProdutoSimples.java
@@ -0,0 +1,20 @@
+package com.github.jrcsouza.fundamentals.m01_fundamentals;
+
+import java.util.Scanner;
+
+public class ProdutoSimples {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+
+        int prod = a * b;
+
+        System.out.println("PROD = " + prod);
+
+        scanner.close();
+    }
+}
