4a848cfe4a8e66c3b7f467e4da5390ce2512c1b1
Junio das Chagas de Souza <juniochagas@gmail.com>
2026-08-18 21:46:18 -0300

    Initial commit

---
+++ src/main/java/com/github/jrcsouza/fundamentals/m01_fundamentals/HelloWorld.java
@@ -0,0 +1,9 @@
+package com.github.jrcsouza.fundamentals.m01_fundamentals;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+
+        System.out.println("Hello World!");
+
+    }
+}
