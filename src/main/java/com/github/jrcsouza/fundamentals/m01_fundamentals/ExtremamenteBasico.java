10981c395df9866d021fdd0f2f8caad876ba5b91
Junio das Chagas de Souza <juniochagas@gmail.com>
2026-08-18 23:09:40 -0300

    feat: resolve Beecrowd 1001 - Extremamente Basico

---
+++ src/main/java/com/github/jrcsouza/fundamentals/m01_fundamentals/ExtremamenteBasico.java
@@ -0,0 +1,21 @@
+package com.github.jrcsouza.fundamentals.m01_fundamentals;
+
+import java.util.Scanner;
+
+public class ExtremamenteBasico {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int x = a + b;
+
+        System.out.println("X = " + x);
+
+        scanner.close();
+
+    }
+
+}
