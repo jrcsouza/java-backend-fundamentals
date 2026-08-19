5c699e32f2267e74a272f5a6ad4810d207597788
Junio das Chagas de Souza <juniochagas@gmail.com>
2026-08-19 13:13:34 -0300

    feat: resolve Beecrowd 1004 - Produto Simples

---
+++ src/main/java/com/github/jrcsouza/fundamentals/m01_fundamentals/SomaSimples.java
@@ -0,0 +1,21 @@
+package com.github.jrcsouza.fundamentals.m01_fundamentals;
+
+import java.util.Scanner;
+
+public class SomaSimples {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+
+        int soma = a + b;
+
+        System.out.println("SOMA = " + soma);
+
+        scanner.close();
+    }
+
+}
