41c1175385179ce09615fadceca6105ccf1ea708
Junio das Chagas de Souza <juniochagas@gmail.com>
2026-08-20 20:45:28 -0300

    feat: resolve Beecrowd 1005 - Media 1

---
+++ src/main/java/com/github/jrcsouza/fundamentals/m01_fundamentals/Media1.java
@@ -0,0 +1,24 @@
+package com.github.jrcsouza.fundamentals.m01_fundamentals;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Media1 {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        scanner.useLocale(Locale.US);
+
+        double a = scanner.nextDouble();
+        double b = scanner.nextDouble();
+
+        double media = (a * 3.5 + b * 7.5) / 11;
+
+        System.out.printf(Locale.US, "MEDIA = %.5f\n", media);
+
+        scanner.close();
+
+
+    }
+}
