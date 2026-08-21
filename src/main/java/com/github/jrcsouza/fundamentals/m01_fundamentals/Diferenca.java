cf53e77fd48eb0cc8943b3d944882df850e24b71
Junio das Chagas de Souza <juniochagas@gmail.com>
2026-08-21 09:55:45 -0300

    feat: resolve Beecrowd 1007 - Diferenca

---
+++ src/main/java/com/github/jrcsouza/fundamentals/m01_fundamentals/Diferenca.java
@@ -0,0 +1,22 @@
+package com.github.jrcsouza.fundamentals.m01_fundamentals;
+
+import java.util.Scanner;
+
+public class Diferenca {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int c = scanner.nextInt();
+        int d = scanner.nextInt();
+
+        int diferenca = (a * b - c * d);
+
+        System.out.println("DIFERENCA = " + diferenca);
+
+
+    }
+}
---
+++ src/main/java/com/github/jrcsouza/fundamentals/m01_fundamentals/Media2.java
@@ -0,0 +1,25 @@
+package com.github.jrcsouza.fundamentals.m01_fundamentals;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Media2 {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        scanner.useLocale(Locale.US);
+
+        double a = scanner.nextDouble();
+        double b = scanner.nextDouble();
+        double c = scanner.nextDouble();
+
+        double media = ((a * 2) + (b * 3) + (c * 5)) / 10;
+
+        System.out.printf(Locale.US, "MEDIA = %.1f\n", media);
+
+        scanner.close();
+
+
+    }
+}
