8231a7dfd6b7567c9b1737ae4edda7f1ec5de123
Junio das Chagas de Souza <juniochagas@gmail.com>
2026-08-25 11:50:26 -0300

    feat: add Beecrowd 1014 - Consumo

--- src/main/java/com/github/jrcsouza/fundamentals/m01_fundamentals/Consumo.java
+++ src/main/java/com/github/jrcsouza/fundamentals/m01_fundamentals/Consumo.java
@@ -1,6 +1,5 @@
 package com.github.jrcsouza.fundamentals.m01_fundamentals;
 
-import java.io.PrintStream;
 import java.util.Locale;
 import java.util.Scanner;
 
@@ -15,6 +14,6 @@ public class Consumo {
         double consumoMedio = x / y;
 
         System.out.printf(Locale.US,"%.3f km/l%n", consumoMedio);
-
+        scanner.close();
     }
 }
