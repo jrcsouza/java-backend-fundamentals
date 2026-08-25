156d887bb23a4277287263d15d52e7afb88f8a34
Junio das Chagas de Souza <juniochagas@gmail.com>
2026-08-25 15:29:12 -0300

    feat: add Beecrowd 1018 - Cedulas

---
+++ src/main/java/com/github/jrcsouza/fundamentals/m01_fundamentals/Cedulas.java
@@ -0,0 +1,36 @@
+package com.github.jrcsouza.fundamentals.m01_fundamentals;
+
+import java.util.Scanner;
+
+public class Cedulas {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int valor = scanner.nextInt();
+
+        System.out.println(valor);
+
+        int notas100 = valor / 100;
+        valor = valor % 100;
+        int notas50 = valor / 50;
+        valor = valor % 50;
+        int notas20 = valor / 20;
+        valor = valor % 20;
+        int notas10 = valor / 10;
+        valor = valor % 10;
+        int notas5 = valor / 5;
+        valor = valor % 5;
+        int notas2 = valor / 2;
+        valor = valor % 2;
+        int notas1 = valor;
+
+        System.out.println(notas100 + " nota(s) de R$ 100,00");
+        System.out.println(notas50 + " nota(s) de R$ 50,00");
+        System.out.println(notas20 + " nota(s) de R$ 20,00");
+        System.out.println(notas10 + " nota(s) de R$ 10,00");
+        System.out.println(notas5 + " nota(s) de R$ 5,00");
+        System.out.println(notas2 + " nota(s) de R$ 2,00");
+        System.out.println(notas1 + " nota(s) de R$ 1,00");
+        scanner.close();
+    }
+}
