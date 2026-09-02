b8f7b70ec85c5f2ab199210936f7a0fa9e730112
Junio das Chagas de Souza <juniochagas@gmail.com>
2026-09-01 23:10:08 -0300

    feat: add Beecrowd 1035 - Teste de seleção 1

--- src/main/java/com/github/jrcsouza/fundamentals/m01_fundamentals/TesteDeSelecao1.java
+++ src/main/java/com/github/jrcsouza/fundamentals/m01_fundamentals/TesteDeSelecao1.java
@@ -4,29 +4,28 @@ import java.util.Scanner;
 
 public class TesteDeSelecao1 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
-            Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int c = scanner.nextInt();
+        int d = scanner.nextInt();
 
-            int a = scanner.nextInt();
-            int b = scanner.nextInt();
-            int c = scanner.nextInt();
-            int d = scanner.nextInt();
+        boolean isBMaiorQueC = b > c;
+        boolean isDMaiorQueA = d > a;
+        boolean isSomaCDMaiorQueSomaAB = (c + d) > (a + b);
+        boolean isCDPositivos = (c > 0) && (d > 0);
+        boolean isAForPar = (a % 2 == 0);
 
-            boolean isBMaiorQueC = b > c;
-            boolean isDMaiorQueA = d > a;
-            boolean isSomaCDMaiorQueSomaAB = (c + d) > (a + b);
-            boolean isCDPositivos = (c > 0) && (d > 0);
-            boolean isAForPar = (a % 2 == 0);
+        if (isBMaiorQueC && isDMaiorQueA && isSomaCDMaiorQueSomaAB && isCDPositivos && isAForPar) {
 
-            if (isBMaiorQueC && isDMaiorQueA && isSomaCDMaiorQueSomaAB && isCDPositivos && isAForPar) {
+            System.out.println("Valores aceitos");
 
-                System.out.println("Valores aceitos");
+        } else {
 
-            } else {
-
-                System.out.println("Valores nao aceitos");
-            }
-            scanner.close();
+            System.out.println("Valores nao aceitos");
+        }
+        scanner.close();
     }
 }
 
@@ -50,5 +49,4 @@ public class TesteDeSelecao1 {
         scanner.close();
     }
 }
-
- */
+*/
